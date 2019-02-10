ccec9a52a77a8ce253c8ace17afc17e76c4648d6
Kevin Lin <kevinlin47@hotmail.com>
2019-02-10 15:32:22 -0500

    Added landing page
    Created views folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require("express");
 var app=express();
 
 app.get("/",function(req,res){
-	res.send("This will be the landing page");
+	res.render("landing");
 });
 
 app.listen(3000,"127.0.0.1",function(){
