f6993a3008fd1caec307d7ec613bc0f096e7e314
Kevin Lin <kevinlin47@hotmail.com>
2019-02-22 16:15:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app=express();
 var bodyParser=require("body-parser");
 var mongoose=require("mongoose");
 var Campground=require("./models/campground");
+var Comment=require("./models/comment");
 var seedDB=require("./seeds")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
@@ -82,7 +83,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 });
 
 app.post("/campgrounds/:id/comments", function(req, res){
-	
+
+	Campground.findById(req.params.id, function(err, campground){
+		if(err)
+		{
+			console.log(err);
+			res.redirect("/campgrounds");
+		}
+		else
+		{
+			Comment.create(req.body.comment, function(err, comment){
+				if(err)
+				{
+					console.log(err);
+				}
+				else
+				{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/"+campground._id);
+				}
+			});
+		}
+	});
 });
 
 app.listen(3000,"127.0.0.1",function(){
