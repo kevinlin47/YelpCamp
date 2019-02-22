ca05aa0096575bac68c1b4f7311a39a6f8f0193e
Kevin Lin <kevinlin47@hotmail.com>
2019-02-22 15:22:12 -0500

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,11 @@ var mongoose=require("mongoose");
 var campgrounSchema=new mongoose.Schema({
 	name: String,
 	image: String,
-	description: String
+	description: String,
+	comments: [{
+		type: mongoose.Schema.Type.ObjectId,
+		ref: "Comment"
+	}]
 });
 
 module.exports=mongoose.model("Campground",campgrounSchema);
