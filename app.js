2aac652e3bbf0a985cb1d754f08f0d2874e02be7
Kevin Lin <kevinlin47@hotmail.com>
2019-02-24 12:53:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
 seedDB();
 
+
+//Passport configuration
+app.use(require("express-session")({
+	secret: "Teemo is evil!",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/",function(req,res){
 	res.render("landing");
 });
