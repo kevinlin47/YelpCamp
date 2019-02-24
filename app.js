d51c61c24b138011c07752965f88741f4898fe1f
Kevin Lin <kevinlin47@hotmail.com>
2019-02-24 14:51:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
 	});
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
 
 	Campground.findById(req.params.id, function(err, campground){
 		if(err)
