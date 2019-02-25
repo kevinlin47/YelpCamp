93380f7d21ac18dcc04e36572237c92ceb97c9c9
Kevin Lin <kevinlin47@hotmail.com>
2019-02-25 06:31:16 -0500

    Update campgrounds.js

---
+++ routes/campgrounds.js
@@ -0,0 +1,53 @@
+app.get("/campgrounds",function(req,res){
+
+	// Get all campgrounds from DB
+	Campground.find({}, function(err,allcampgrounds){
+		if (err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("campgrounds/index",{campgrounds:allcampgrounds});
+		}
+	})
+});
+
+app.post("/campgrounds",function(req,res){
+	var name=req.body.name;
+	var image=req.body.image;
+	var description=req.body.description;
+	var newCampGround={name:name,image:image,description:description};
+
+	//Create a new campground and save to DB
+	Campground.create(newCampGround, function(err,newCampground){
+		if(err)
+		{
+			console.log(err)
+		}
+		else
+		{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+app.get("/campgrounds/new", function(req,res){
+	res.render("campgrounds/new");
+});
+
+//SHOW- shows more info about one campground
+app.get("/campgrounds/:id", function(req,res)
+{	
+	//find the camoground with provided ID
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("campgrounds/show", {campground:foundCampground});
+		}
+	});
+});
