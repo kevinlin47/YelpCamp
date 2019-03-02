1922012bf2c9e38bb9bd32906b14aaa539a8624c
Kevin Lin <kevinlin47@hotmail.com>
2019-03-02 11:02:27 -0500

    Add edit comment route and buttton

--- routes/comments.js
+++ routes/comments.js
@@ -50,6 +50,10 @@ router.post("/", isLoggedIn, function(req, res){
 	});
 });
 
+router.get("/:comment_id/edit", function(req, res){
+
+});
+
 //middleware
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated())
