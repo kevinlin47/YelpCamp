c22eaaa2902e5837463a27ccec844c6a52c35243
Kevin Lin <kevinlin47@hotmail.com>
2019-03-02 15:40:31 -0500

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -51,7 +51,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //Comment Edit 
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err)
 		{
@@ -100,6 +100,33 @@ function isLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+	if(req.isAuthenticated())
+	{
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err)
+			{
+				res.redirect("back");
+			}
+			else
+			{	
+				if(foundComment.author.id.equals(req.user._id))
+				{
+					next();
+				}
+				else
+				{
+					res.redirect("back");
+				}
+			}
+		});
+	}
+	else
+	{
+		red.redirect("back");
+	}
 }
 
 module.exports=router;
