73a7500aa4cd2fc3fc4b0cce6802ebcb44ef30ee
Kevin Lin <kevinlin47@hotmail.com>
2019-03-02 16:28:13 -0500

    Refactored middleware

--- routes/comments.js
+++ routes/comments.js
@@ -2,9 +2,10 @@ var express=require("express");
 var router=express.Router({mergeParams: true});
 var Campground=require("../models/campground")
 var Comment=require("../models/comment");
+var middleware=require("../middleware");
 
 //Comments New
-router.get("/new", isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function(req, res){
 	Campground.findById(req.params.id, function(err, foundCampground){
 		if(err)
 		{
@@ -18,7 +19,7 @@ router.get("/new", isLoggedIn, function(req, res){
 });
 
 //Comments Create
-router.post("/", isLoggedIn, function(req, res){
+router.post("/", middleware.isLoggedIn, function(req, res){
 
 	Campground.findById(req.params.id, function(err, campground){
 		if(err)
@@ -51,7 +52,7 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //Comment Edit 
-router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err)
 		{
@@ -65,7 +66,7 @@ router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 });
 
 //Comment Update
-router.put("/:comment_id", checkCommentOwnership, function(req, res){
+router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
 		if(err)
 		{
@@ -80,7 +81,7 @@ router.put("/:comment_id", checkCommentOwnership, function(req, res){
 
 
 //Comment Delete
-router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err, deleteComment){
 		if(err)
 		{
@@ -92,41 +93,5 @@ router.delete("/:comment_id", checkCommentOwnership, function(req, res){
 		}
 	});
 });
-
-//middleware
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated())
-	{
-		return next();
-	}
-	res.redirect("/login");
-}
-
-function checkCommentOwnership(req, res, next){
-	if(req.isAuthenticated())
-	{
-		Comment.findById(req.params.comment_id, function(err, foundComment){
-			if(err)
-			{
-				res.redirect("back");
-			}
-			else
-			{	
-				if(foundComment.author.id.equals(req.user._id))
-				{
-					next();
-				}
-				else
-				{
-					res.redirect("back");
-				}
-			}
-		});
-	}
-	else
-	{
-		red.redirect("back");
-	}
-}
 
 module.exports=router;
