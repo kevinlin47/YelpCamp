604912dc71ef6aefebc7bf1b7161f0f28f9a6da0
Kevin Lin <kevinlin47@hotmail.com>
2019-03-06 06:35:11 -0500

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,28 +30,36 @@ router.get("/",function(req,res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", middleware.isLoggedIn, function(req,res){
-	var name=req.body.name;
-	var image=req.body.image;
-	var description=req.body.description;
-	var price=req.body.price;
-	var author={
-		id: req.user._id,
-		username: req.user.username
-	}
-	var newCampGround={name:name,image:image,description:description, author:author, price:price};
-
-	//Create a new campground and save to DB
-	Campground.create(newCampGround, function(err,newCampground){
-		if(err)
-		{
-			console.log(err)
-		}
-		else
-		{
-			res.redirect("/campgrounds");
-		}
-	});
+router.post("/", middleware.isLoggedIn, function(req, res){
+  // get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var author = {
+      id: req.user._id,
+      username: req.user.username
+  };
+  geocoder.geocode(req.body.location, function (err, data) {
+    if (err || !data.length) {
+      console.log(err);
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
+    }
+    var lat = data[0].latitude;
+    var lng = data[0].longitude;
+    var location = data[0].formattedAddress;
+    var newCampground = {name: name, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
+  });
 });
 
 //NEW - show form to create new campground
