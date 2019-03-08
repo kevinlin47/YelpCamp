a1c53fe83a7ef53bcc305908e4d98637e5283a9d
Kevin Lin <kevinlin47@hotmail.com>
2019-03-08 15:39:01 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express=require("express");
 var router=express.Router();
 var passport=require("passport");
 var User=require("../models/user");
+var Campground=require("../models/campground");
 var middleware=require("../middleware");
 
 //root route
