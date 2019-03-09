4ff1dbb0ad8667b0e7167da9894170eb5b6e0faf
Kevin Lin <kevinlin47@hotmail.com>
2019-03-09 16:14:50 -0500

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -144,4 +144,8 @@ router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
 	});
 });
 
+function escapeRegex(text){
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g,"\\$&");
+}
+
 module.exports=router;
