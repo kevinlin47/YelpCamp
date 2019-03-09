521503ccc49e6ffc5a47b94e54eada82df9c3e19
Kevin Lin <kevinlin47@hotmail.com>
2019-03-09 11:57:34 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -85,7 +85,63 @@ router.get("/forgot", function(req, res){
 	res.render("forgot");
 });
 
+router.post("/forgot", function(req, res, next){
+	async.waterfall([
+		function(done){
+			crypto.randomBytes(20, function(err, buf){
+				var token=buf.toString("hex");
+				done(err, token);
+			});
+		},
+		function(token, done){
+			User.findOne({email: req.body.email}, function(err, user){
+				if(!user)
+				{
+					req.flash("error", "No account with that email address exists.");
+					return res.redirect("/forgot");
+				}
 
+				user.resetPasswordToken=token;
+				user.resetPasswordExpires=Date.now()+3600000; //1 hour
+
+				user.save(function(err){
+					done(err, token, user);
+				});
+			});
+		},
+		function(token, user, done){
+			var smtpTransport=nodemailer.createTransport({
+				service: "Gmail",
+				auth: {
+					user: "kevin.java.lin@gmail.com",
+					pass: process.env.GMAILPW
+				}
+			});
+			var mailOptions={
+				to: user.email,
+				from: "kevin.java.lin@gmail.com",
+				subject: "Password Reset",
+				text: "You are receving this because you (or someone else) have requested the rest of the password for your account.\n\n"
+				+ "Please click on the following link or paste this into your browswer to complete the process:\n\n" +
+				"http://"+ req.header.host+ "/reset/" + token + "\n\n" +
+				"If you did not request this, please ignore this email and your password will remain unchanged.\n"
+			};
+			smtpTransport.sendMail(mailOptions, function(err){
+				console.log("mail sent");
+				req.flash("success", "An e-mail has been sent to " + user.email + " with further instructions.");
+				done(err, "done");
+			});
+		}], function(err){
+			if(err)
+			{
+				return next(err);
+			}
+			else
+			{
+				res.redirect("/forgot");
+			}
+		});
+});
 
 // User profile
 router.get("/users/:id", function(req, res){
