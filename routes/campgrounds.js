3c8cf1d5c59a8be3eea3982311c4dd49db31392f
Kevin Lin <kevinlin47@hotmail.com>
2019-03-10 13:32:54 -0400

    dsfd

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,7 +14,7 @@ var imageFilter=function(req, file, cb){
   if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)){
     return cb(new Error("Only image files are allowed."), false);
   }
-  cb.(null,true);
+  cb(null,true);
 };
 var upload=multer({storage: storage, fileFilter: imageFilter});
 
