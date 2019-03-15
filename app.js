3095a5bb89b8137faae18d014084be516b977be4
Kevin Lin <kevinlin47@hotmail.com>
2019-03-15 12:22:28 -0400

    add database url for heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var campgroundRoutes=require("./routes/campgrounds");
 var commentRoutes=require("./routes/comments");
 var indexRoutes=require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
@@ -47,6 +48,14 @@ app.use(indexRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(3000,"127.0.0.1",function(){
-	console.log("YelpCamp Server Started");
-});
+ //Local host connection
+/*  app.listen(3000,"127.0.0.1",function(){
+ 	console.log("YelpCamp Server Started");
+ }); */
+
+ //Heroku host connection
+ app.listen(process.env.PORT || 3000, "0.0.0.0", function(){
+ 	console.log("YelpCamp Server Started");
+ });
+
+
